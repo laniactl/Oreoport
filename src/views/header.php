b8471b94a14b7177d80b45d92860f71a29d1d49a
magalimarchand <Saphir22>
2017-11-06 15:51:43 -0500

    Modification du nom des links dans la navigation du header

--- src/views/header.php
+++ src/views/header.php
@@ -55,9 +55,8 @@
                     <a href="<?= URL?>/index" class="dropdown-toggle" data-toggle="dropdown" role="button"
                        aria-haspopup="true" aria-expanded="false">État des vols <span class="caret"></span></a>
                     <ul class="dropdown-menu inverse-dropdown">
-                        <li><a href= "#" class="linkdepart">Départs</a></li>
-                        <li><a href= "#" class="linkarrivee">Arrivées</a></li>
-<!--                        <li><a href="--><?//= URL?><!--/index" class="linkarrivee">Arrivées</a></li>-->
+                        <li><a href="<?= URL ?>/index" class="linkdepart">Départs</a></li>
+                        <li><a href="<?= URL ?>/index" class="linkarrivee">Arrivées</a></li>
                         <li role="separator" class="divider"></li>
                         <li><button type="button" class="btn btn-info"
                                     data-toggle="modal" data-target="#modalSMS">Alertes SMS</button></li>
@@ -105,7 +104,7 @@
 
 <!-- TITRE  -->
 <div >
-    <h5 class="titre">État des vols en départ </h5>
+    <h5 class="titre">ÉTAT DES VOLS</h5>
 </div>
 
 
