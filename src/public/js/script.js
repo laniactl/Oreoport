404afe7f59c0edb75c349b7f5b0e4156722d48ae
magalimarchand <Saphir22>
2017-11-06 15:17:17 -0500

    supression bar latéral alert hoho script.js

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -22,9 +22,4 @@ $(document).ready(function () {
         height2 = $('.main').height()
         htmlbodyHeightUpdate()
     });
-
-  $('linkarrivee').click(function() {
-    alert('ho ho ho');
-  });
-
 });
