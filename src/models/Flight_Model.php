f7d74382a3e42cbd1394591fc45379084b063e50
patrice <patrice.carle.qc@gmail.com>
2017-11-10 13:54:31 -0500

    POST ou GET ne passe pas en DEBUG....

--- src/models/Flight_Model.php
+++ src/models/Flight_Model.php
@@ -12,14 +12,14 @@ class Flight_Model extends Model
         parent::__construct();
     }
 
-    public function listeDepart($valreqToday){
+    public function listeDepart($valreqToday)
+    {
         // Query des departs.
-        if ($valreqToday == "today"){
+        if ($valreqToday == "today") {
             $_dateArrive = date('Y-m-d'); //'2017-11-04';
 
-        }
-        else {
-            $_dateArrive = date('Y-m-d',time()+86400);//'2017-11-05';
+        } else {
+            $_dateArrive = date('Y-m-d', time() + 86400);//'2017-11-05';
         }
         //$_dateArrive = '2017-11-04';
         $_villeDepart = 'YUL';
@@ -61,35 +61,30 @@ class Flight_Model extends Model
     }
 
 
-
-
-
-
     public function listeArrivee($valreqToday)
     {
-        if ($valreqToday == "today"){
+        if ($valreqToday == "today") {
             $_dateArrive = date('Y-m-d');//'2017-11-04';
+        } else {
+            $_dateArrive = date('Y-m-d', time() + 86400);//'2017-11-05';
         }
-        else {
-            $_dateArrive = date('Y-m-d', time()+86400);//'2017-11-05';
-        }
-            //$_dateArrive = '2017-11-03';
-            $_villeDest = 'YUL';
+        //$_dateArrive = '2017-11-03';
+        $_villeDest = 'YUL';
 
-            $ctmt = $this->db->prepare("SELECT COUNT(*) AS RecordCount
+        $ctmt = $this->db->prepare("SELECT COUNT(*) AS RecordCount
                           FROM
                           `oreoport`.`vols_details`
                           INNER JOIN `oreoport`.`vols`
                           ON (`vols_details`.`num_vols` = `vols`.`num_vols`)
                           WHERE `vols`.`ville_destination` = :ville AND `vols_details`.`date_arrivee` = :datearrive ;");
-            $ctmt->bindParam(':datearrive', $_dateArrive);
-            $ctmt->bindParam(':ville', $_villeDest);
-            $ctmt->execute();
-            $result = $ctmt->fetchall();
-            $recordCount = $result[0]['RecordCount'];
+        $ctmt->bindParam(':datearrive', $_dateArrive);
+        $ctmt->bindParam(':ville', $_villeDest);
+        $ctmt->execute();
+        $result = $ctmt->fetchall();
+        $recordCount = $result[0]['RecordCount'];
 
 
-            $stmt = $this->db->prepare("SELECT vols_details.vols_details_id, vols_details.num_vols, vols_details.heure_est_depart,
+        $stmt = $this->db->prepare("SELECT vols_details.vols_details_id, vols_details.num_vols, vols_details.heure_est_depart,
                                     vols_details.heure_est_arrivee, vols_details.vol_status, compagnie.compagnie_nom,
                                     nom_aeroport_ville.nom_ville FROM oreoport.vols 
                                     INNER JOIN oreoport.compagnie ON (vols.compagnie_id = compagnie.compagnie_id)
@@ -97,10 +92,10 @@ class Flight_Model extends Model
                                     INNER JOIN oreoport.nom_aeroport_ville ON (vols.ville_provenance = nom_aeroport_ville.code_ville)
                                     WHERE (vols_details.date_arrivee = :datearrive AND ville_destination = :ville)
                                     ORDER BY " . $_GET['jtSorting'] . " LIMIT " . $_GET['jtStartIndex'] . "," . $_GET['jtPageSize']);
-            $stmt->bindParam(':datearrive', $_dateArrive);
-            $stmt->bindParam(':ville', $_villeDest);
-            $stmt->execute();
-            $result = $stmt->fetchAll();
+        $stmt->bindParam(':datearrive', $_dateArrive);
+        $stmt->bindParam(':ville', $_villeDest);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
 
 
         //Return result to jTable
@@ -111,4 +106,103 @@ class Flight_Model extends Model
         print json_encode($jTableResult);
 
     }
+
+//    public function listeArriveeFiltre($valreqToday)
+//    {
+//        $aRechercher = $_POST["recherche"];
+//        $aFiltrer = $_POST["searchId"];
+//
+//        if ($valreqToday == "today") {
+//            $_dateArrive = date('Y-m-d');//'2017-11-04';
+//        } else {
+//            $_dateArrive = date('Y-m-d', time() + 86400);//'2017-11-05';
+//        }
+//        //$_dateArrive = '2017-11-03';
+//        $_villeDest = 'YUL';
+//
+//        $ctmt = $this->db->prepare("SELECT COUNT(*) AS RecordCount
+//                          FROM
+//                          `oreoport`.`vols_details`
+//                          INNER JOIN `oreoport`.`vols`
+//                          ON (`vols_details`.`num_vols` = `vols`.`num_vols`)
+//                          WHERE `vols`.`ville_destination` = :ville AND `vols_details`.`date_arrivee` = :datearrive AND `vols_details`.`num_vols` = :recherche ;");
+//        $ctmt->bindParam(':datearrive', $_dateArrive);
+//        $ctmt->bindParam(':ville', $_villeDest);
+//        $ctmt->bindParam(':recherche', $aRechercher);
+//        $ctmt->execute();
+//        $result = $ctmt->fetchall();
+//        $recordCount = $result[0]['RecordCount'];
+//
+//
+//        $stmt = $this->db->prepare("SELECT vols_details.vols_details_id, vols_details.num_vols, vols_details.heure_est_depart,
+//                                    vols_details.heure_est_arrivee, vols_details.vol_status, compagnie.compagnie_nom,
+//                                    nom_aeroport_ville.nom_ville FROM oreoport.vols
+//                                    INNER JOIN oreoport.compagnie ON (vols.compagnie_id = compagnie.compagnie_id)
+//                                    INNER JOIN oreoport.vols_details ON (vols_details.num_vols = vols.num_vols)
+//                                    INNER JOIN oreoport.nom_aeroport_ville ON (vols.ville_provenance = nom_aeroport_ville.code_ville)
+//                                    WHERE (vols_details.date_arrivee = :datearrive AND vols_details.num_vols = :recherchevol )
+//                                    ORDER BY " . $_GET['jtSorting'] . " LIMIT " . $_GET['jtStartIndex'] . "," . $_GET['jtPageSize']);
+//        $stmt->bindParam(':datearrive', $_dateArrive);
+//        $stmt->bindParam(':ville', $_villeDest);
+//        $ctmt->bindParam(':recherchevol', $aRechercher);
+//        $stmt->execute();
+//        $result = $stmt->fetchAll();
+//
+//        //Return result to jTable
+//        $jTableResult = array();
+//        $jTableResult['Result'] = "OK";
+//        $jTableResult['TotalRecordCount'] = $recordCount;
+//        $jTableResult['Records'] = $result;
+//        print json_encode($jTableResult);
+//    }
+//
+//    public function listeDepartFiltre($valreqToday)
+//    {
+//        $aRechercher = $_POST["recherche"];
+//        $aFiltrer = $_POST["searchId"];
+//
+//        if ($valreqToday == "today") {
+//            $_dateArrive = date('Y-m-d');//'2017-11-04';
+//        } else {
+//            $_dateArrive = date('Y-m-d', time() + 86400);//'2017-11-05';
+//        }
+////$_dateArrive = '2017-11-03';
+//        $_villeDest = 'YUL';
+//
+//        $ctmt = $this->db->prepare("SELECT COUNT(*) AS RecordCount
+//                          FROM
+//                          `oreoport`.`vols_details`
+//                          INNER JOIN `oreoport`.`vols`
+//                          ON (`vols_details`.`num_vols` = `vols`.`num_vols`)
+//                          WHERE `vols`.`ville_destination` = :ville AND `vols_details`.`date_arrivee` = :datearrive AND `vols_details`.`num_vols` = :recherche ;");
+//        $ctmt->bindParam(':datearrive', $_dateArrive);
+//        $ctmt->bindParam(':ville', $_villeDest);
+//        $ctmt->bindParam(':recherche', $aRechercher);
+//        $ctmt->execute();
+//        $result = $ctmt->fetchall();
+//        $recordCount = $result[0]['RecordCount'];
+//
+//
+//        $stmt = $this->db->prepare("SELECT vols_details.vols_details_id, vols_details.num_vols, vols_details.heure_est_depart,
+//                                    vols_details.heure_est_arrivee, vols_details.vol_status, compagnie.compagnie_nom,
+//                                    nom_aeroport_ville.nom_ville FROM oreoport.vols
+//                                    INNER JOIN oreoport.compagnie ON (vols.compagnie_id = compagnie.compagnie_id)
+//                                    INNER JOIN oreoport.vols_details ON (vols_details.num_vols = vols.num_vols)
+//                                    INNER JOIN oreoport.nom_aeroport_ville ON (vols.ville_provenance = nom_aeroport_ville.code_ville)
+//                                    WHERE (vols_details.date_arrivee = :datearrive AND vols_details.num_vols = :recherchevol )
+//                                    ORDER BY " . $_GET['jtSorting'] . " LIMIT " . $_GET['jtStartIndex'] . "," . $_GET['jtPageSize']);
+//        $stmt->bindParam(':datearrive', $_dateArrive);
+//        $stmt->bindParam(':ville', $_villeDest);
+//        $ctmt->bindParam(':recherchevol', $aRechercher);
+//        $stmt->execute();
+//        $result = $stmt->fetchAll();
+//
+////Return result to jTable
+//        $jTableResult = array();
+//        $jTableResult['Result'] = "OK";
+//        $jTableResult['TotalRecordCount'] = $recordCount;
+//        $jTableResult['Records'] = $result;
+//        print json_encode($jTableResult);
+//    }
+
 }
